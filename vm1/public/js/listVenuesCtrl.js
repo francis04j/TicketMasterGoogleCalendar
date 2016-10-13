081dc2431bb8d71021a3c37ce1ad5b77674d1b9a
francis04j <francis04j@gmail.com>
2016-10-13 01:34:59 +0100

    list venues page controoler update

---
+++ vm1/public/js/listVenuesCtrl.js
@@ -0,0 +1,7 @@
+
+var listVenuesCtrl = angular.module('listVenuesCtrl', ['geolocation', 'gservice']);
+listVenuesCtrl.controller('listVenuesCtrl', function($scope, $http, $rootScope, geolocation, gservice){
+//show the map,
+//list the venues from the DB
+
+});
